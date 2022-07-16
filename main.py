ca9b848eda1f4543c1b2ed4178f3e7e55758fac6
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2022-07-16 09:52:22 +0200

    Import turtle method Check Image import

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import turtle
+import pandas
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+screen = turtle.Screen()
+screen.title("Vienna District Quiz")
+image = "./Maps/vienna_de17.gif"
+screen.addshape(image)
 
+turtle.shape(image)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.exitonclick()

c5bae833a8181c6b0c4cb7d0b2696b650b52275a
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2022-07-16 10:35:00 +0200

    write down todo-list

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import turtle
 import pandas
 
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
 screen = turtle.Screen()
 screen.title("Vienna District Quiz")
 image = "./Maps/vienna_de17.gif"
 screen.addshape(image)
 turtle.shape(image)
+
+# TODO: Convert the guess to Title-Case
+# TODO: Check if guess is among the 23 districts
+# TODO: Write correct guesses onto the map
+# TODO: Use a loop that allows continuous guessing.
+# TODO: Record the correct guesses in a list
+# TODO: Keep track of the score
+
 
 answer_district = screen.textinput(title="Rate die Bezirke", prompt="Welche Bezirke gibt es noch?")
 print(answer_district)

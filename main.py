9143ee535527570d7484b052a5cf71fc6f8b5669
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2022-07-20 20:33:02 +0200

    - Convert guess to Title-Case (manipulate input) - Add Game Over function (exit if input is None) -- Add Solution Screen

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import turtle
 import pandas
 
+# Screen Configuration
 screen = turtle.Screen()
 screen.title("Vienna District Quiz")
-image = "./Maps/vienna_de17.gif"
+image = "./Maps/vienna_de15.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# TODO: Convert the guess to Title-Case
-# TODO: Check if guess is among the 23 districts
-# TODO: Write correct guesses onto the map
-# TODO: Use a loop that allows continuous guessing.
-# TODO: Record the correct guesses in a list
-# TODO: Keep track of the score
+# CONSTANTS and initial variables
+game_on = True
 
+# TODO: Check if guess is among the 23 districts (pandas?)
+# TODO: Write correct guesses onto the map (Turtle position on right guess)
+# TODO: Use a loop that allows continuous guessing. (While gameOn)
+# TODO: Record the correct guesses in a list (create entry, no doubles allowed)
+# TODO: Keep track of the score (count the correct entries)
 
-answer_district = screen.textinput(title="Rate die Bezirke", prompt="Welche Bezirke gibt es noch?")
-print(answer_district)
-screen.mainloop()
+while(game_on):
+    answer_district = screen.textinput(title="Rate die Bezirke", prompt="Welche Bezirke gibt es noch?")
+    if answer_district is None:
+        game_on = False
+    else:
+        print(answer_district.title())
+
+solution_image = "./Maps/vienna_de17.gif"
+screen.addshape(solution_image)
+turtle.shape(solution_image)
+
+screen.exitonclick()
 
 

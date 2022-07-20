4bfd153f414198e065d5fc877a9b8e4d2c09a089
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2022-07-20 22:29:38 +0200

    - simplify the adding of pictures (function)

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import turtle
 import pandas
 
+
+def show_picture(picture_path):
+    screen.addshape(picture_path)
+    turtle.shape(picture_path)
+
+
 # Screen Configuration
 screen = turtle.Screen()
 screen.title("Vienna District Quiz")
-image = "./Maps/vienna_de15.gif"
-screen.addshape(image)
-turtle.shape(image)
+
+# Show blank map
+show_picture(picture_path="./Maps/vienna_de15.gif")
 
 # CONSTANTS and initial variables
 game_on = True
@@ -17,17 +23,14 @@ game_on = True
 # TODO: Record the correct guesses in a list (create entry, no doubles allowed)
 # TODO: Keep track of the score (count the correct entries)
 
-while(game_on):
+while (game_on):
     answer_district = screen.textinput(title="Rate die Bezirke", prompt="Welche Bezirke gibt es noch?")
     if answer_district is None:
         game_on = False
     else:
         print(answer_district.title())
 
-solution_image = "./Maps/vienna_de17.gif"
-screen.addshape(solution_image)
-turtle.shape(solution_image)
+# Show map with solution
+show_picture(picture_path="./Maps/vienna_de17.gif")
 
 screen.exitonclick()
-
-

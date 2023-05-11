ddddadeedb43fd997c74e737360ed59545368b86
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2023-05-11 20:55:25 +0200

    Finish Base Game Cleanup Code

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import turtle
 import pandas as pd
 
+
 def show_picture(picture_path):
+    """
+    Diese Funktion zeigt ein beliebiges Bild auf Turtles.
+    Parameter ist entsprechend der Pfad dorthin.
+    """
     screen.addshape(picture_path)
     turtle.shape(picture_path)
+
+
 def write_district(dict_entry, answer):
+    """
+    Aus einer Bezirks oder Ortsliste im CSV-Format wird ein Dictionary gebastelt.
+    :param dict_entry:
+    :param answer:
+    :return:
+    """
     # Create a turtle object
     t = turtle.Turtle()
     # Hide the turtle
@@ -23,6 +36,8 @@ def write_district(dict_entry, answer):
     # Write the text at the turtle's position
     text = answer
     t.write(text, align="center", font=("Arial", 8, "normal"))
+
+
 def create_dict(path):
     # import districts from csv-file and put them into a data list
     # in the same time an empty list should be created for guessing.
@@ -36,6 +51,8 @@ def create_dict(path):
         coords = [info[2], info[3]]
         dict[name] = coords
     return dict
+
+
 # Screen Configuration
 screen = turtle.Screen()
 screen.title("Vienna District Quiz")
@@ -45,25 +62,26 @@ show_picture(picture_path="./Maps/vienna_de15.gif")
 
 # CONSTANTS and initial variables
 game_on = True
-
+answers = []
 district_dict = create_dict('Data/district_names.csv')
 
 while (game_on):
-    answer_district = screen.textinput(title="Rate die Bezirke", prompt="Bitte gib einen Bezirk ein!")
+    answer_district = screen.textinput(title="Rate die Bezirke", prompt=f"Bitte gib einen Bezirk ein!"
+                                                                        f"\nNoch {23 - len(answers)} übrig.")
     if answer_district == "":
         game_on = False
     elif answer_district in district_dict:
-        # Todo: Check if guess is among the 23 districts
+        if answer_district in answers:
+            print("Oh nein, das hatten wir ja schon!")
+            continue
 
         # Insert the right guess onto the map
         write_district(district_dict, answer_district)
 
+        # Put the right answer into a separate list for reference
+        answers.append(answer_district)
     else:
         print(answer_district.title())
-
-# TODO: Record the correct guesses in a list (create entry, no doubles allowed)
-# TODO: Keep track of the score (count the correct entries)
-
 
 # Show map with solution
 show_picture(picture_path="./Maps/vienna_de17.gif")
